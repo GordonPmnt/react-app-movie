72f5ca8b17455b10022828a3eca708faf5c25370
Gordon Preumont <preumontgr@gmail.com>
2020-05-02 15:37:56 +0200

    form fields

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
 
-function App() {
+const App = () => {
   return (
     <>
-      
+      <h1>Please fill in your favorite movie</h1>
+      <form>
+        <fieldset>
+          <legend>Movie informations</legend>
+          <div>
+            <label htmlFor="movieName">Movie name</label>
+            <input
+              type="text"
+              id="movieName"
+              name="lastname"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="posterUrl">Movie poster</label>
+            <input
+              type="text"
+              id="posterUrl"
+              name="posterUrl"
+              placeholder="Please type the poster URL"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="comments">Comments</label>
+            <input
+              type="textarea"
+              id="comments"
+              name="comments"
+            />
+          </div>
+          <hr />
+          <div>
+            <input type="submit" value="Send" />
+          </div>
+        </fieldset>
+      </form>
     </>
   );
 }
